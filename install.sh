e84ef6ab78b72ac4e1c99db17d5f3a974ffcb2f8
Roghan <roghanthomasbehm@gmail.com>
2025-12-27 18:34:29 +1000

    Add install target to CMakeLists

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "building terminyl (${BUILD_TYPE})..."
 cmake -S . -B build -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
 cmake --build build --parallel
 
-printf "One of the lessons of war is that institutions, while powerful and long-lasting, are often not insuperably rigid _if the emergency in great enough_.\n\n\n\n\n"
+printf "\n\nOne of the lessons of war is that institutions, while powerful and long-lasting, are often not insuperably rigid _if the emergency in great enough_.\n\n"
 sudo cmake --install build
 
-echo "done, run 'terminyl' to use."
+echo "done, run 'terminyl <source file>' to use."
